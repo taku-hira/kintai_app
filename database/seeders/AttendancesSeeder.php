984d595a0b0bb15703caec4fe8cbbcb87f0ae375
goldenslumber <goldenslumbers0902@gmail.com>
2022-02-21 23:47:14 +0900

    #32 勤怠ダミー作成

---
+++ database/seeders/AttendancesSeeder.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AttendancesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('attendances')->insert([
+            [
+                'user_id' => '1',
+                'date' => '2022-01-03',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-03 08:00:00',
+                'end_time' => '2022-01-03 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-04',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-04 08:00:00',
+                'end_time' => '2022-01-04 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-05',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-05 08:00:00',
+                'end_time' => '2022-01-05 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-06',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-06 08:00:00',
+                'end_time' => '2022-01-06 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-07',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-07 08:00:00',
+                'end_time' => '2022-01-07 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-10',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-10 08:00:00',
+                'end_time' => '2022-01-10 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-11',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-11 08:00:00',
+                'end_time' => '2022-01-11 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-12',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-12 08:00:00',
+                'end_time' => '2022-01-12 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-13',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-13 08:00:00',
+                'end_time' => '2022-01-13 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-14',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-14 08:00:00',
+                'end_time' => '2022-01-14 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-17',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-17 08:00:00',
+                'end_time' => '2022-01-17 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-18',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-18 08:00:00',
+                'end_time' => '2022-01-18 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-19',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-19 08:00:00',
+                'end_time' => '2022-01-03 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-20',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-20 08:00:00',
+                'end_time' => '2022-01-20 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-21',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-21 08:00:00',
+                'end_time' => '2022-01-21 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-24',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-24 08:00:00',
+                'end_time' => '2022-01-24 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-25',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-25 08:00:00',
+                'end_time' => '2022-01-25 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-26',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-26 08:00:00',
+                'end_time' => '2022-01-26 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-27',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-27 08:00:00',
+                'end_time' => '2022-01-27 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-28',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-28 08:00:00',
+                'end_time' => '2022-01-28 17:00:00',
+                'break_time' => '1:00',
+            ],
+            [
+                'user_id' => '1',
+                'date' => '2022-01-31',
+                'schedule_type' => '1',
+                'start_time' => '2022-01-31 08:00:00',
+                'end_time' => '2022-01-31 17:00:00',
+                'break_time' => '1:00',
+            ],
+        ]);
+    }
+}
