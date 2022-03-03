93d3a91f43d9cb5fa548c32a153d81e6380dbed1
goldenslumber <goldenslumbers0902@gmail.com>
2022-03-03 18:23:43 +0900

    #52 管理者側から勤怠データ編集

--- app/Http/Controllers/Admin/UserAttendanceRecordController.php
+++ app/Http/Controllers/Admin/UserAttendanceRecordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Shift;
+use App\Models\Attendance;
 
 class UserAttendanceRecordController extends Controller
 {
@@ -18,7 +19,29 @@ class UserAttendanceRecordController extends Controller
             $record[$attendance_record->date] = $attendance_record;
         }
 
-        return view('admin.user_attendance_record.index', compact('record'));
+        return view('admin.user_attendance_record.index', compact('record', 'user_id'));
+    }
+
+    public function create(Request $request, $id)
+    {
+        $date = $request->date;
+        $shift_lists = Shift::all();
+        return view('admin.user_attendance_record.create', compact('date', 'shift_lists', 'id'));
+    }
+
+    public function store(Request $request, $id)
+    {
+        $attendance_record = new Attendance();
+
+        $attendance_record->user_id = $id;
+        $attendance_record->shift_id = $request->shift;
+        $attendance_record->date = $request->date;
+        $attendance_record->start_time = $request->date . '-' . $request->start_time;
+        $attendance_record->end_time = $request->date . '-' . $request->end_time;
+        $attendance_record->break_time = $request->break_time;
+        $attendance_record->save();
+
+        return redirect()->route('admin.user_attendance_record.index', ['id' => $id])->with('flash_message', '編集完了しました');
     }
 
     public function edit($user_id, $attendance_id)
